f49421571e32873a3841ab949bcc6350b383fd2f
fwilbor <franklin.cfg@gmail.com>
2020-05-14 13:19:37 -0400

    set initital grocerylist

---
+++ java/groceryList.java
@@ -0,0 +1,27 @@
+public class GroceryList {
+	
+	private String name;
+	
+	
+	public GroceryList() {
+		
+	}
+	
+	public GroceryList(String name) {
+		super();
+		this.name = name;
+		
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	
+
+	
+}
